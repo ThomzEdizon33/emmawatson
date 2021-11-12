8c3b2beae10b394519b7b2d44348e59a20993796
ThomzEdizon <90401221+ThomzEdizon@users.noreply.github.com>
2021-11-12 16:48:10 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -8,7 +8,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
-    if data == "aboutℹ️":
+    if data == "about":
         await query.message.edit_text(
             text = f"<b>○ Owner: <a href='tg://user?id={OWNER_ID}'>Edizon💡</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Ask movies➡️ : <a href='https://t.me/malayalammoviestg1'>Here♻️</a>\n○ New OTT 🎬 : @TGmovie1\n○ Movie Suggestions 💫 : @mallumoviesuggest </b>",
             disable_web_page_preview = True,
