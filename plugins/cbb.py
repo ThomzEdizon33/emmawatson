4c8267c3e1aa31bb1f2b622479f81417811c257a
ThomzEdizon <90401221+ThomzEdizon@users.noreply.github.com>
2021-12-02 10:38:59 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Owner: <a href='tg://user?id={OWNER_ID}'>Edizon💡</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Ask movies➡️ : <a href='https://t.me/malayalammoviestg1'>Here♻️</a>\n○ New OTT 🎬 : @TGmovie1\n○ Movie Suggestions 💫 : @mallumoviesuggest </b>",
+            text = f"<b>○ Owner: <a href='tg://user?id={OWNER_ID}'>Edizon💡</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Ask movies➡️ : <a href='https://t.me/malayalammoviestg1'>Here♻️</a>\n○ New OTT 🎬 : @TGmovie1\n○ source code : https://github.com/ThomzEdizon/emmawatson </b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
